60e3e3a29f31a8cfc299d5047c1363c936f9c9ef
Ali Cihan Demir <demircihanali@gmail.com>
2021-07-12 17:18:57 +0300

    Ödev 5
    Ödev 5' in cevapları eklendi.

--- Ödev/odevler.sql
+++ Ödev/odevler.sql
@@ -73,6 +73,27 @@ WHERE length(country) = 5;
 SELECT COUNT(*) FROM city
 WHERE city ILIKE '%r';
 
+--ÖDEV 5
+
+--5.1
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+--5.2
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+--5.3
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
+
 
 
 
