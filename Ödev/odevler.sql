128aec4b79a206d289012cf8d72f67b9731f4e83
Ali Cihan Demir <demircihanali@gmail.com>
2021-07-12 15:14:20 +0300

    Ödev 2
    Ödev 2' nin cevapları eklendi.

--- Ödev/odevler.sql
+++ Ödev/odevler.sql
@@ -18,6 +18,22 @@ WHERE first_name = 'Mary';
 
 --1.5
 SELECT * FROM film
-WHERE lenght < 50 AND NOT(rental_rate = 2.99 OR rental_rate = 4.99)
+WHERE lenght < 50 AND NOT(rental_rate = 2.99 OR rental_rate = 4.99);
+
+
+-- ÖDEV 2
+
+--2.1
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+--2.2
+SELECT first_name, last_name FROM actor
+WHERE first_name IN ('Penelope','Nick','Ed');
+
+--2.3
+SELECT * FROM film
+WHERE (rental_rate IN (0.99,2.99,4.99)) AND (replacement_cost IN (12.99,15.99,28.99));
+
 
 
