202cb4d166bf3594be6001e7768a1f4b3d7a0205
Ali Cihan Demir <demircihanali@gmail.com>
2021-07-17 16:20:42 +0300

    Ödev 9
    Ödev 9 ' un cevapları eklendi.

--- Ödev/odevler.sql
+++ Ödev/odevler.sql
@@ -230,3 +230,17 @@ WHERE email = 'csuter14@wsj.com';
 
 DELETE FROM employee
 WHERE name = 'Mandy';
+
+--ÖDEV 9
+
+--9.1
+SELECT city, country FROM country
+INNER JOIN city ON country.country_id = city.country_id;
+
+--9.2
+SELECT payment_id, first_name, last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id;
+
+--9.3
+SELECT rental_id, first_name, last_name FROM rental
+INNER JOIN customer ON rental.customer_id = customer.customer_id;
